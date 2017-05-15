9d5f0f2687d6d631d36eaa82907b67c7dc414c53
System Administrator <root@Andres-MacBook-Pro.local>
2017-05-15 08:33:05 -0300

    Adicionado campo Strong. Codigo semi-refatorado

--- app.js
+++ app.js
@@ -16,58 +16,60 @@ img1.src = 'img/bg-post-fb.jpg';
 function reset() {
 
 	ctx.drawImage(img1, 0, 0);
-
 }
 
 var link = document.querySelector('#download');
     
 link.addEventListener('click', function(ev) {
     link.href = canvas.toDataURL();
-    link.download = "mypainting.png";
+    link.download = obj.traducao.split(' ').join('-').toLowerCase()+"-"+obj.strong.toUpperCase()+".png";
 }, false);
 
+var obj = {
+	palavra : '',
+	traducao : '',
+	strong : ''
+}
 
-var titulo;
-var traducao;
-
-document.getElementById('palavra').addEventListener('keyup', function() {
-	var stringTitle = document.getElementById('palavra').value;
-	
-	titulo = stringTitle;
-
-	reset();
-
+function desenhaPalavra(obj){
 	ctx.fillStyle = '#000';
 	ctx.font = "60px Times New Roman";
 	ctx.textAlign = "center";
-	ctx.fillText(stringTitle,(canvas.width / 2), (canvas.height / 2));
+	ctx.fillText(obj.palavra,(canvas.width / 2), (canvas.height / 2));
+}
 
-	if (traducao) {
-		ctx.fillStyle = '#282828';
-		ctx.font = "16px Arial";
-		ctx.textAlign = "center";
-		ctx.fillText(traducao,(canvas.width / 2), 300);
-	}
-
-});
-
-document.getElementById('traducao').addEventListener('keyup', function() {
-	var stringTitle = document.getElementById('traducao').value;
-	
-	traducao = stringTitle;
-
-	reset();
-
-	if (titulo) {
-		ctx.fillStyle = '#000';
-		ctx.font = "60px Times New Roman";
-		ctx.textAlign = "center";
-		ctx.fillText(titulo,(canvas.width / 2), (canvas.height / 2));
-	}
-
+function desenhaTraducao(obj){
 	ctx.fillStyle = '#282828';
 	ctx.font = "16px Arial";
 	ctx.textAlign = "center";
-	ctx.fillText(stringTitle,(canvas.width / 2), 300);
+	ctx.fillText(obj.traducao,(canvas.width / 2), 300);
+}
 
+function desenhaStrong(obj){
+	ctx.fillStyle = '#282828';
+	ctx.font = "14px Arial";
+	ctx.textAlign = "center";
+	ctx.fillText("Strong: " + obj.strong.toUpperCase(),(canvas.width / 2), 330);
+}
+
+function desenhaAll(obj) {
+	reset();
+	desenhaPalavra(obj);
+	desenhaTraducao(obj);
+	desenhaStrong(obj)
+}
+
+document.getElementById('palavra').addEventListener('keyup', function() {
+	obj.palavra = document.getElementById('palavra').value;
+	desenhaAll(obj)
+});
+
+document.getElementById('traducao').addEventListener('keyup', function() {
+	obj.traducao = document.getElementById('traducao').value;
+	desenhaAll(obj)
+});
+
+document.getElementById('strong').addEventListener('keyup', function() {
+	obj.strong = document.getElementById('strong').value;
+	desenhaAll(obj)
 });
